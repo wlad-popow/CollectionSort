778cc5f4fcac78809c0a48d721635c8b669d1736
mihanick <mihanick@gmail.com>
2015-11-03 18:48:54 +0600

    Add stubs for static person comparer methods

---
+++ Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/PersonComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omgtu.CollectionSortExcercise
+{
+    public sealed static class PersonComparer 
+    {
+        public static int CompareByName(Person leftPerson, Person rightPerson)
+        {
+            //TODO: Добавить реализацию сравнения по именам
+            throw new NotImplementedException();
+        }
+
+        public static int CompareBySurname(Person leftPerson, Person rightPerson)
+        {
+            //TODO: Добавить реализацию сравнения по именам
+            throw new NotImplementedException();
+        }
+
+        public static int CompareByGroupNameSurname(Person leftPerson, Person rightPerson)
+        {
+            //TODO: Добавить реализацию сравнения по именам
+            throw new NotImplementedException();
+        }
+    }
+}
