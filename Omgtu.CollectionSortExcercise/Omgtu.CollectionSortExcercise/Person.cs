1a8584ef979b61230f708180eb3b991f7ecf7c9c
mihanick <mihanick@gmail.com>
2015-11-03 18:47:15 +0600

    Add initialization from string

--- Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/Person.cs
+++ Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/Person.cs
@@ -6,12 +6,33 @@ using System.Threading.Tasks;
 
 namespace Omgtu.CollectionSortExcercise
 {
-    class Person
+    public class Person
     {
         public string Name { get; set; }
 
         public string Surname { get; set; }
 
         public string Group { get; set; }
+
+        /// <summary>
+        /// Инициализирует пустого человека без имени
+        /// </summary>
+        public Person()
+        {
+            this.Name = string.Empty;
+            this.Surname = string.Empty;
+            this.Group = string.Empty;
+        }
+
+        public Person(string nameSurnameGroup)
+        {
+            string[] splittedStrings = nameSurnameGroup.Split(' ');
+            if (splittedStrings.Length !=3)
+                throw new ArgumentException("Строка не соответствует шаблону");
+
+            this.Name = splittedStrings[0];
+            this.Surname = splittedStrings[1];
+            this.Group = splittedStrings[2];
+        }
     }
 }
