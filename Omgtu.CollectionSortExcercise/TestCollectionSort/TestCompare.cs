b465efad9b1019d13ba17450224fc9172599ff29
mihanick <mihanick@gmail.com>
2015-11-03 15:01:18 +0600

    Add test solution

---
+++ Omgtu.CollectionSortExcercise/TestCollectionSort/TestCompare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCollectionSort
+{
+    [TestClass]
+    public class TestCompare
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
