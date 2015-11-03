3e6524f08030c52c3b5c2fe8b49ec1f04cb40024
mihanick <mihanick@gmail.com>
2015-11-03 18:50:25 +0600

    Add tests for comparing by name, surname, groupNameSurname

--- Omgtu.CollectionSortExcercise/TestCollectionSort/TestCompare.cs
+++ Omgtu.CollectionSortExcercise/TestCollectionSort/TestCompare.cs
@@ -1,14 +1,76 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
 
 namespace TestCollectionSort
+namespace Omgtu.CollectionSortExcercise
 {
     [TestClass]
     public class TestCompare
     {
+        internal List<Person> students;
+        internal string sourcePath = @"group.txt";
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestPersonCollectionSortByName()
         {
+            ReadGroup();
+
+            List<string> expectedList = new List<string>();
+            foreach (Person student in students)
+                expectedList.Add(student.Name);
+            expectedList.Sort();
+
+            students.Sort(PersonComparer.CompareByName);
+
+            for (int i = 0; i < students.Count; i++)
+                Assert.AreEqual(expectedList[i], students[i].Name);
+        }
+
+        [TestMethod]
+        public void TestPersonCollectionSortBySurName()
+        {
+            ReadGroup();
+
+            List<string> expectedList = new List<string>();
+            foreach (Person student in students)
+                expectedList.Add(student.Surname);
+            expectedList.Sort();
+
+            students.Sort(PersonComparer.CompareBySurname);
+
+            for (int i = 0; i < students.Count; i++)
+                Assert.AreEqual(expectedList[i], students[i].Surname);
+        }
+
+        [TestMethod]
+        public void TestPersonCollectionSortByGroupNameSurname()
+        {
+            ReadGroup();
+
+            List<string> expectedList = new List<string>();
+            foreach (Person student in students)
+            {
+                string expected = student.Group.Substring(0, 4);
+                expected += student.Name.Substring(0, 4);
+                expected += student.Surname.Substring(0, 4);
+
+                expectedList.Add(expected);
+            }
+            expectedList.Sort();
+
+            students.Sort(PersonComparer.CompareByGroupNameSurname);
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                string actual = students[i].Group.Substring(0, 4);
+                actual += students[i].Name.Substring(0, 4);
+                actual += students[i].Surname.Substring(0, 4);
+
+                Assert.AreEqual(expectedList[i], actual);
+            }
         }
     }
 }
