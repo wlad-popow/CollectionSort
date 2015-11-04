11179fdb021ec50f3b5ae8b6e2fe7d270f2b29cc
mihanick <mihanick@gmail.com>
2015-11-04 14:26:16 +0600

    Add sample program for reading and sorting persons in textfile

--- Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/Program.cs
+++ Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace Omgtu.CollectionSortExcercise
     {
         static void Main(string[] args)
         {
+            List<Person> students = new List<Person>();
+            string fileFullPath = Path.GetFullPath(@"group.txt");
+
+            if (!File.Exists(fileFullPath))
+                throw new FileNotFoundException();
+
+            string line;
+            using (StreamReader file = new StreamReader(fileFullPath))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    Person student = new Person(line);
+                    students.Add(student);
+                }
+            }
+
+            students.Sort(PersonComparer.CompareByGroupNameSurname);
+
+            foreach (Person student in students)
+                Console.WriteLine(student);
+
+            Console.ReadKey();
         }
     }
 }
