5742478e296887b3ed7297ede48789e5cab35671
mihanick <mihanick@gmail.com>
2015-11-04 12:48:37 +0600

    Add compare by name realization

--- Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/PersonComparer.cs
+++ Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/PersonComparer.cs
@@ -10,20 +10,26 @@ namespace Omgtu.CollectionSortExcercise
     {
         public static int CompareByName(Person leftPerson, Person rightPerson)
         {
-            //TODO: Добавить реализацию сравнения по именам
-            throw new NotImplementedException();
+            if (leftPerson == null)
+                if (rightPerson == null)
+                    return 0;
+                else
+                    return -1;
+            else
+                return leftPerson.Name.CompareTo(rightPerson.Name);
+
         }
 
         public static int CompareBySurname(Person leftPerson, Person rightPerson)
         {
             //TODO: Добавить реализацию сравнения по именам
-            throw new NotImplementedException();
+            return 0;
         }
 
         public static int CompareByGroupNameSurname(Person leftPerson, Person rightPerson)
         {
             //TODO: Добавить реализацию сравнения по именам
-            throw new NotImplementedException();
+            return 0;
         }
     }
 }
