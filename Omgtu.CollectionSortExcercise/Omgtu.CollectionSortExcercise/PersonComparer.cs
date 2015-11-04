03213406a8a62ed6776a2305e77565619932c0a0
mihanick <mihanick@gmail.com>
2015-11-04 13:58:17 +0600

    All comparisons throw - need to implement

--- Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/PersonComparer.cs
+++ Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/PersonComparer.cs
@@ -10,59 +10,20 @@ namespace Omgtu.CollectionSortExcercise
     {
         public static int CompareByName(Person leftPerson, Person rightPerson)
         {
-            if (leftPerson == null && rightPerson == null)
-                return 0;
-
-            if (leftPerson == null)
-                return -1;
-
-            if (rightPerson == null)
-                return 1;
-
-            return leftPerson.Name.CompareTo(rightPerson.Name);
+            //TODO: Добавить реализацию сравнения по именам
+            throw new NotImplementedException();
         }
 
         public static int CompareBySurname(Person leftPerson, Person rightPerson)
         {
-            if (leftPerson == null && rightPerson == null)
-                return 0;
-
-            if (leftPerson == null)
-                return -1;
-
-            if (rightPerson == null)
-                return 1;
-
-            return leftPerson.Surname.CompareTo(rightPerson.Surname);
+            //TODO: Добавить реализацию сравнения по именам
+            throw new NotImplementedException();
         }
 
         public static int CompareByGroupNameSurname(Person leftPerson, Person rightPerson)
         {
-            if (leftPerson == null && rightPerson == null)
-                return 0;
-
-            if (leftPerson == null)
-                return -1;
-
-            if (rightPerson == null)
-                return 1;
-
-            int resulCompareGroup = 
-                leftPerson.Group.Substring(0,4).
-                    CompareTo(rightPerson.Group.Substring(0,4));
-            if (resulCompareGroup != 0)
-                return resulCompareGroup;
-
-            int resultCompareName = leftPerson.Name.CompareTo(rightPerson.Name);
-            if (resultCompareName != 0)
-                return resultCompareName;
-
-            int resultCompareSurname = leftPerson.Surname.CompareTo(rightPerson.Surname);
-            if (resultCompareSurname != 0)
-                return resultCompareSurname;
-
-            return 0;
-
+            //TODO: Добавить реализацию сравнения по именам
+            throw new NotImplementedException();
         }
     }
 }
