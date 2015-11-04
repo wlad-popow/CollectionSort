279e90f67f6119055a5b6737efb082dceba432ac
mihanick <mihanick@gmail.com>
2015-11-04 13:55:35 +0600

    Add all realization of comparisons

--- Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/PersonComparer.cs
+++ Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/PersonComparer.cs
@@ -6,30 +6,63 @@ using System.Threading.Tasks;
 
 namespace Omgtu.CollectionSortExcercise
 {
-    public static class PersonComparer 
+    public static class PersonComparer
     {
         public static int CompareByName(Person leftPerson, Person rightPerson)
         {
-            if (leftPerson == null)
-                if (rightPerson == null)
-                    return 0;
-                else
-                    return -1;
-            else
-                return leftPerson.Name.CompareTo(rightPerson.Name);
+            if (leftPerson == null && rightPerson == null)
+                return 0;
 
+            if (leftPerson == null)
+                return -1;
+
+            if (rightPerson == null)
+                return 1;
+
+            return leftPerson.Name.CompareTo(rightPerson.Name);
         }
 
         public static int CompareBySurname(Person leftPerson, Person rightPerson)
         {
-            //TODO: Добавить реализацию сравнения по именам
-            return 0;
+            if (leftPerson == null && rightPerson == null)
+                return 0;
+
+            if (leftPerson == null)
+                return -1;
+
+            if (rightPerson == null)
+                return 1;
+
+            return leftPerson.Surname.CompareTo(rightPerson.Surname);
         }
 
         public static int CompareByGroupNameSurname(Person leftPerson, Person rightPerson)
         {
-            //TODO: Добавить реализацию сравнения по именам
+            if (leftPerson == null && rightPerson == null)
+                return 0;
+
+            if (leftPerson == null)
+                return -1;
+
+            if (rightPerson == null)
+                return 1;
+
+            int resulCompareGroup = 
+                leftPerson.Group.Substring(0,4).
+                    CompareTo(rightPerson.Group.Substring(0,4));
+            if (resulCompareGroup != 0)
+                return resulCompareGroup;
+
+            int resultCompareName = leftPerson.Name.CompareTo(rightPerson.Name);
+            if (resultCompareName != 0)
+                return resultCompareName;
+
+            int resultCompareSurname = leftPerson.Surname.CompareTo(rightPerson.Surname);
+            if (resultCompareSurname != 0)
+                return resultCompareSurname;
+
             return 0;
+
         }
     }
 }
